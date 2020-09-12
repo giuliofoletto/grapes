f8e63e692a0f753f12fdfec1a2723f6aef862495
Giulio Foletto <giulio.foletto@outlook.com>
2020-09-12 18:55:59 +0200

    Add square brackets operators

--- qflow/core.py
+++ qflow/core.py
@@ -18,6 +18,13 @@ class Graph:
     def __init__(self):
         self.nodes = {}
 
+    def __getitem__(self, key):
+        return self.nodes[key].value
+
+    def __setitem__(self, key, value):
+        self.nodes[key].value = value
+        self.nodes[key].has_value = True
+
     def add_node(self, node):
         self.nodes.update({node.name: node})
 
