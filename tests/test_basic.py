8a1809ac0c0b7b2e6f3941758d0232499e0c02d6
Giulio Foletto <giulio.foletto@outlook.com>
2020-09-12 20:12:22 +0200

    Add test of diamond graph

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -26,6 +26,18 @@ def test_simplified_input():
     res = qflow.execute_graph_from_context(g, {"a":1, "b": 2, "f": 12, "op_e": lambda x,y : x+y, "op_f": lambda x,y : x*y, "op_g": lambda x,y : x-y}, "g")
     assert res["g"] == -9
 
+def test_diamond():
+    g = qflow.Graph()
+    g.add_node(("a"))
+    g.add_node("b", "op_b", "a")
+    g.add_node("c", "op_c", "b")
+    g.add_node("d", "op_d", "b")
+    g.add_node("e", "op_e", "c", "d")
+
+    res = qflow.execute_graph_from_context(g, {"a":1, "op_b": lambda x: 2*x, "op_c": lambda x: 2*x, "op_d": lambda x: 2*x, "op_e": lambda x,y : x-y}, "e")
+    assert res["e"] == 0
+
 if __name__ == "__main__":
     test_simple()
     test_simplified_input()
+    test_diamond()
