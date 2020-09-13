8d96b7bf19e8ee0c4ceadfd7961ddaa6ddf67680
Giulio Foletto <giulio.foletto@outlook.com>
2020-09-13 12:40:15 +0200

    Move graphviz tests to different file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -37,35 +37,7 @@ def test_diamond():
     res = qflow.execute_graph_from_context(g, {"a":1, "op_b": lambda x: 2*x, "op_c": lambda x: 2*x, "op_d": lambda x: 2*x, "op_e": lambda x,y : x-y}, "e")
     assert res["e"] == 0
 
-def test_graphviz():
-    g = qflow.Graph(name = "graph1")
-    g.add_node("e", "op_e", "a", "b")
-    g.add_node("f", "op_f", "c", "d")
-    g.add_node("g", "op_g", "e", "f")
-    gv = g.get_graphviz_digraph()
-    gv.view()
-
-def test_graphviz_with_values():
-    g = qflow.Graph(name = "graph_values")
-    g.add_node("e", "op_e", "a", "b")
-    g.add_node("f", "op_f", "c", "d")
-    g.add_node("g", "op_g", "e", "f")
-    g.set_internal_state({"a":1, "b": 2, "f": 12, "op_e": lambda x,y : x+y, "op_f": lambda x,y : x*y, "op_g": lambda x,y : x-y})
-    gv = g.get_graphviz_digraph()
-    gv.view()
-
-def test_graphviz_attrs():
-    g = qflow.Graph(name = "graph2")
-    g.add_node("e", "op_e", "a", "b")
-    g.add_node("f", "op_f", "c", "d")
-    g.add_node("g", "op_g", "e", "f")
-    gv = g.get_graphviz_digraph(rankdir = "LR")
-    gv.view()
-
 if __name__ == "__main__":
     test_simple()
     test_simplified_input()
     test_diamond()
-    test_graphviz()
-    test_graphviz_with_values()
-    test_graphviz_attrs()
---
+++ tests/test_graphviz.py
@@ -0,0 +1,35 @@
+import pytest
+import qflow
+
+def test_graphviz():
+    g = qflow.Graph()
+    g.add_node("e", "op_e", "a", "b")
+    g.add_node("f", "op_f", "c", "d")
+    g.add_node("g", "op_g", "e", "f")
+    gv = g.get_graphviz_digraph(name = "test_graphviz")
+    gv.render()
+    gv.save()
+
+def test_graphviz_with_values():
+    g = qflow.Graph()
+    g.add_node("e", "op_e", "a", "b")
+    g.add_node("f", "op_f", "c", "d")
+    g.add_node("g", "op_g", "e", "f")
+    g.set_internal_state({"a":1, "b": 2, "f": 12, "op_e": lambda x,y : x+y, "op_f": lambda x,y : x*y, "op_g": lambda x,y : x-y})
+    gv = g.get_graphviz_digraph(name = "test_graphviz_with_values")
+    gv.render()
+    gv.save()
+
+def test_graphviz_attrs():
+    g = qflow.Graph()
+    g.add_node("e", "op_e", "a", "b")
+    g.add_node("f", "op_f", "c", "d")
+    g.add_node("g", "op_g", "e", "f")
+    gv = g.get_graphviz_digraph(name = "test_graphviz_attrs", rankdir = "LR")
+    gv.render()
+    gv.save()
+
+if __name__ == "__main__":
+    test_graphviz()
+    test_graphviz_with_values()
+    test_graphviz_attrs()
