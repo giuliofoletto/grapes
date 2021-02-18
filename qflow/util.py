76529980d37c5e9e20e3c4ca1e8482faedc355ec
Giulio Foletto <giulio.foletto@outlook.com>
2021-02-18 15:40:29 +0100

    Improve utility function that executes the graph

--- qflow/util.py
+++ qflow/util.py
@@ -1,5 +1,24 @@
-def execute_graph_from_context(graph, context, targets = []):
-    graph.clear_values()
+def execute_graph_from_context(graph, context, *targets):
+    """Execute a graph up to a target given a context
+
+    Parameters
+    ----------
+    graph : qflow Graph
+        Graph of the computation
+    context : dict
+        Dictionary of the initial state of the computation (input)
+    targets : strings (or keys in the graph)
+        Indicator of what to compute (desired output)
+    
+    Returns
+    -------
+    dict
+        Final state of the computation (context is not modified in place, a deep copy is used)
+    """
+
+    graph.clear_values(keep_operations = True)
     graph.set_internal_state(context)
-    graph.execute_to_targets(targets)
-    return graph.get_internal_state()
+    graph.execute_to_targets(*targets)
+    final_context = graph.get_internal_state(only_data = True)
+
+    return final_context
