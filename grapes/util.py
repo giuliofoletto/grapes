8417ab5994f69a4783552cfa75a83f060c1280c9
Giulio Foletto <giulio.foletto@outlook.com>
2021-07-04 14:19:06 +0200

    Wrap now supports input as kwargs

--- grapes/util.py
+++ grapes/util.py
@@ -86,22 +86,37 @@ def context_from_json_file(file_name):
     return data
 
 
-def wrap_graph_with_function(graph, input_keys, *targets):
+def wrap_graph_with_function(graph, input_keys, *targets, input_as_kwargs=True):
     # Copy graph so as not to pollute the original
     operational_graph = copy.deepcopy(graph)
+    if input_as_kwargs:
+        def specific_function(**kwargs):
+            # Use for loop rather than dict comprehension because it is a more basic operation
+            for key in input_keys:
+                operational_graph[key] = kwargs[key]
+            operational_graph.execute_to_targets(*targets)
+            list_of_values = operational_graph.get_list_of_values(targets)
+            # Clear values so that the function can be called again
+            operational_graph.clear_values()
+            if len(list_of_values) == 1:
+                return list_of_values[0]
+            else:
+                return list_of_values
+    else:
+        input_keys = list(input_keys)
 
-    def specific_function(*args):
-        # Use for loop rather than dict comprehension because it is a more basic operation
-        for i in range(len(input_keys)):
-            operational_graph[input_keys[i]] = args[i]
-        operational_graph.execute_to_targets(*targets)
-        list_of_values = operational_graph.get_list_of_values(targets)
-        # Clear values so that the function can be called again
-        operational_graph.clear_values()
-        if len(list_of_values) == 1:
-            return list_of_values[0]
-        else:
-            return list_of_values
+        def specific_function(*args):
+            # Use for loop rather than dict comprehension because it is a more basic operation
+            for i in range(len(input_keys)):
+                operational_graph[input_keys[i]] = args[i]
+            operational_graph.execute_to_targets(*targets)
+            list_of_values = operational_graph.get_list_of_values(targets)
+            # Clear values so that the function can be called again
+            operational_graph.clear_values()
+            if len(list_of_values) == 1:
+                return list_of_values[0]
+            else:
+                return list_of_values
     return specific_function
 
 
