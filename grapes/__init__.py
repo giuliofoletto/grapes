bcc57b10eae9708faa72564cb99f5d762295bc43
Giulio Foletto <giulio.foletto@outlook.com>
2024-04-02 19:28:24 +0200

    Eliminate unnecessary imports

--- grapes/__init__.py
+++ grapes/__init__.py
@@ -1,5 +1,15 @@
-from .core import *
-from .util import *
-from .visualize import *
+from .core import Graph
+from .util import (
+    check_feasibility_of_execution,
+    context_from_file,
+    context_from_json_file,
+    context_from_toml_file,
+    execute_graph_from_context,
+    get_execution_subgraph,
+    json_from_graph,
+    lambdify_graph,
+    wrap_graph_with_function,
+)
+from .visualize import get_graphviz_digraph
 
 __version__ = "0.8.0"
